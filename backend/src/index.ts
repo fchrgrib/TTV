30e45ae58466c576a6eb2492cfc9656b010b1b11
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-06-23 17:46:27 +0700

    fix: finalize backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,22 +8,11 @@ const port = process.env.PORT || 8080
 const db = require('./services/db.connection')
 app.use(express.json())
 app.use(cors({credentials: true, origin:['*'], methods: ['GET', 'POST', 'PUT', 'DELETE'],}))
-
 db.connect.connect()
 MainRouter(app)
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
-
-app.post('/', (req, res) => {
-    db.connect.query('INSERT INTO ttv(tekanan_darah, denyut_nadi, suhu_tubuh, laju_nafas, tinggi_badan, berat_badan) VALUES(?,?,?,?,?,?)',["tekanan_darah", "sdfsdfs","sdfgsdgsdg","sdgfsdgsg","sdfsdf","sdgfsdgsd"], (err: any, result: any) => {
-        if (err) {
-            console.log(err)
-            res.send('error')
-        } else {
-            res.send('success')
-        }
-    })
 })
 
 app.listen(port, () => {
