99ead6e74d2bb5e63ca3ea978a19e533378d20ea
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-06-25 00:12:37 +0700

    style: header recapitulation

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,11 +7,13 @@
     <div class = "w-full h-full col ps-20 pe-20 pt-20">
       <p class="text-4xl font-bold text-secondary400 select-none">TTV Recapitulation</p>
       <div class="w-full h-auto mt-10 flex justify-between">
-        <div class="flex w-auto h-50">
+        <div class="flex w-96 h-50">
           <input type="text" placeholder="Search" class="rounded-xl w-96 h-12 ps-3 pe-3">
-
+          <button class="ml-3 bg-primary500 hover:bg-secondary400 p-2 rounded-xl font-bold text-base">
+            Search
+          </button>
         </div>
-        <button class="bg-primary500 hover:bg-secondary400 p-2 rounded-md font-bold text-base">ADD TTV</button>
+        <button class="bg-primary500 hover:bg-secondary400 p-2 ps-10 pe-10 rounded-xl font-bold text-base">ADD TTV +</button>
       </div>
     </div>
   </div>
